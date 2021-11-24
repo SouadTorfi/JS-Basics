96b4e18b8b2e6f40c714b1a1cfeab4bf8415c777
SouadTorfi <souad.torfi99@gmail.com>
2021-11-24 13:29:50 +0200

    completed Step 3 and 4

---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,5 @@
+let name = prompt("what is your name: ");
+let surname = prompt(" your lastname: ");
+let city = prompt("your city: ");
+alert("Hello my name is : " +name+ "\n" + "My lastname is : " +surname+ "\n" + "My city : " +city);
+
