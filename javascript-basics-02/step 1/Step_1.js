5e3695d7355cdc265570b7bdd3073b60c2392c3d
SouadTorfi <souad.torfi99@gmail.com>
2021-11-24 19:39:56 +0200

    completed Step 1

---
+++ javascript-basics-02/step 1/Step_1.js
@@ -0,0 +1,10 @@
+
+var image=document.querySelector("#image1");
+
+var change_images = function(){
+    
+   
+     image.src = "images/image1_2.jpg";
+    }
+
+    image.addEventListener("mouseover",change_images);
