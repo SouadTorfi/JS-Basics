7ae8f00dce160e904d3fce2ce6e1168db1b855e2
SouadTorfi <souad.torfi99@gmail.com>
2021-11-27 12:40:20 +0200

    update step 8

--- javascript-basics-01/Step_8.js
+++ javascript-basics-01/Step_8.js
@@ -1,5 +1,9 @@
-let age = prompt("Enter your age");
-if(age >= 18)
- alert("you are over 18");
-else
- alert("you are under 18");
+ function ask () {
+    var age = document.getElementById('age').value;
+    if(age >= 18)
+    alert("you are over 18");
+   else
+    alert("you are under 18");
+}
+
+
